296d47b898805fc452bbb44ddcbc8da619a6f352
Juan Rendon <jp_rendon93@hotmail.com>
2022-05-31 22:10:53 -0500

    Cambiar el test para añadir un test que de falso

--- test_unittest.py
+++ test_unittest.py
@@ -3,12 +3,16 @@ import unittest   # The test framework
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     #Este es cierto y deber ser correcto
-    def test_true(self):
+    def test_20_true(self):
         self.assertEqual(inc_dec.check(51000), 40800)
 
     #Este es falso no y debe pasar el test
-    def test_false(self):
+    def test_5_true(self):
         self.assertEqual(inc_dec.check(10000), 9500)
+
+    #Este es falso no y debe pasar el test
+    def test_20_false(self):
+        self.assertEqual(inc_dec.check(51000), 50000)
 
 if __name__ == '__main__':
     unittest.main()
